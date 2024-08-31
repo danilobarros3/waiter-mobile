c1e89e6adb685d47a9af9f2a9da5ecfceae92119
danilobarros3 <danilo.lionsoft@gmail.com>
2024-08-31 18:04:27 -0300

    :tada: init: Initial commit

---
+++ src/Main/index.tsx
@@ -0,0 +1,30 @@
+import { Categories } from "../components/Categories";
+import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
+import { Text } from "../components/Text";
+import {
+  Container,
+  CategoriesContainer,
+  MenuContainer,
+  Footer,
+  FooterContainer,
+} from "./style";
+
+export function Main() {
+  return (
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
+  );
+}
